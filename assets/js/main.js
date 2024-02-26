b70f02d8ee34622ee6f32b2c111ee8555f6b252b
ali tarichi <tarichiali@gmail.com>
2024-02-26 17:16:36 +0300

    header color fixed with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER PRODUCTS ===============*/
 
